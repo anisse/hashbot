4f521181f68e3dc8a11dd356d15a157f369e2ff9
Anisse Astier <anisse@astier.eu>
2012-04-09 23:55:09 +0200

    Put everything in a function

--- hashbot.py
+++ hashbot.py
@@ -60,35 +60,37 @@ twitter_sample_parameters = {
         'stall_warnings': 'true',
         }
 
-# Twitter stream API on the "sample" feed
-# twitter pretends it gives ~1% of the tweet at a given time, I think it's much lower
-# they must adapt its verbosity/rate level to load.
-r = requests.post('https://stream.twitter.com/1/statuses/sample.json',
-        data=twitter_sample_parameters,
-        config=r_config,
-        hooks={'pre_request': oauth_hook })
+def bot_main():
+    # Twitter stream API on the "sample" feed
+    # twitter pretends it gives ~1% of the tweet at a given time, I think it's much lower
+    # they must adapt its verbosity/rate level to load.
+    r = requests.post('https://stream.twitter.com/1/statuses/sample.json',
+            data=twitter_sample_parameters,
+            config=r_config,
+            hooks={'pre_request': oauth_hook })
 
-c = RateCounter()
+    c = RateCounter()
 
-for line in r.iter_lines():
-    if line: # filter out keep-alive new lines
-        text = str(line)
-        tweet = json.loads(text)
-        if 'user' in tweet and 'screen_name' in tweet['user'] and 'text' in tweet:
-            #print(tweet['user']['screen_name'] +": " + tweet['text'])
-            if filter_tweet(tweet['text']): # we could be (much?) faster by filtering before loading json
-                print("Matched tweet!")
+    for line in r.iter_lines():
+        if line: # filter out keep-alive new lines
+            text = str(line)
+            tweet = json.loads(text)
+            if 'user' in tweet and 'screen_name' in tweet['user'] and 'text' in tweet:
+                #print(tweet['user']['screen_name'] +": " + tweet['text'])
+                if filter_tweet(tweet['text']): # we could be (much?) faster by filtering before loading json
+                    print("Matched tweet!")
+                    #print(json.dumps(tweet, indent=4))
+                    retweet(tweet['id_str'])
+                pass
+            else:
                 #print(json.dumps(tweet, indent=4))
-                retweet(tweet['id_str'])
-            pass
-        else:
-            #print(json.dumps(tweet, indent=4))
-            pass
-        if 'warning' in tweet:
-            print("==== WARNING !!! ====")
-            print(json.dumps(tweet, indent=4))
-            print("==== WARNING !!! ====")
+                pass
+            if 'warning' in tweet:
+                print("==== WARNING !!! ====")
+                print(json.dumps(tweet, indent=4))
+                print("==== WARNING !!! ====")
 
-        c.increment()
+            c.increment()
 
-
+if  __name__ == '__main__':
+    bot_main()
